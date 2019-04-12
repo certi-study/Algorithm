a075f9f227a5d4dabf5844dab206e89dfd6cd538
jaejunha <dreamline91@naver.com>
2019-04-12 23:08:04 +0900

    best solution: bsearch

--- jaejun/stl/bsearch.cpp
+++ jaejun/stl/bsearch.cpp
@@ -3,8 +3,8 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-#define N 10000000
-#define M 10000000
+#define N (1 << 22)
+#define M 1 << 20
 
 /* STL에 있는 것 */
 int compare(void const *first, void const *second)
@@ -53,7 +53,6 @@ int bst_op1(int *arr, int key, int size) {
 	return -1;
 }
 
-
 /* 최적화 2 - "=="의 경우는 극히 드문 케이스라 가장 아래 배치 */
 int bst_op2(int *arr, int key, int size) {
 	register int *p = arr;
@@ -100,57 +99,148 @@ int bst_op4(int *arr, int key, int size) {
 	while (r - l > 1) {
 		m = (l + r) >> 1;
 		if (p[m] <= key)
-		    l = m;
+			l = m;
 		else
 			r = m;
 	}
-	
-	if(p[l] == key)
-	    return l;
+
+	if (p[l] == key)
+		return l;
 	else
-	    return -1;
+		return -1;
+}
+
+
+/* 최적화 5 - 반복문 제거 */
+/* reference: https://stackoverflow.com/questions/20024634/how-can-i-optimize-binary-search */
+int bst_op5(int *arr, int key, int size) {
+	register int *p = arr;
+	register int m = N >> 1;
+	register int l = -1;
+
+	if (p[m] < key) // 21
+		l = N - m;
+	m = m >> 1;
+	if (p[l + m] < key) //20
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key) //15
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key) // 10
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key) // 5
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+	m = m >> 1;
+	if (p[l + m] < key)
+		l += m;
+
+
+	if (l > N or p[l] != key)
+		return -1;
 }
 
 int main() {
-	register int *p = (int*)malloc(sizeof(int)*N);
+//	srand((unsigned)time(NULL));
+	int *p = (int*)malloc(sizeof(int)*N);
+	int *q = (int*)malloc(sizeof(int)*M);
 	for (register int i = 0; i < N; i++)
 		*p++ = i;
+	for (register int i = 0; i < M; i++)
+		*q++ = rand() % N;
+
 	p -= N;
+	q -= M;
 
 	int start;
-	int key = 50;
 	int *find;
-    
-    cout<<endl;
-    
-	start = clock();
-	for (register int i = 0; i < M; i++)
-		find = (int*)bsearch(&key, p, N, sizeof(int), compare);
-	cout << "STL: "<< clock() - start << endl;
+
+	cout << endl;
 
 	start = clock();
 	for (register int i = 0; i < M; i++)
-		*find = bst(p, key, N);
-	cout << "No op: " <<clock() - start << endl;
+		find = (int*)bsearch(q + i, p, N, sizeof(int), compare);
+	cout << "STL: " << clock() - start << endl;
 
 	start = clock();
 	for (register int i = 0; i < M; i++)
-		*find = bst_op1(p, key, N);
+		*find = bst(p, *(q + i), N);
+	
+	cout << "No op: " << clock() - start << endl;
+
+	start = clock();
+	for (register int i = 0; i < M; i++)
+		*find = bst_op1(p, *(q + i), N);
 	cout << "Op1: " << clock() - start << endl;
 
 	start = clock();
 	for (register int i = 0; i < M; i++)
-		*find = bst_op2(p, key, N);
+		*find = bst_op2(p, *(q + i), N);
 	cout << "Op2: " << clock() - start << endl;
-	
+
 	start = clock();
 	for (register int i = 0; i < M; i++)
-		*find = bst_op3(p, key, N);
+		*find = bst_op3(p, *(q + i), N);
 	cout << "Op3: " << clock() - start << endl;
 
 	start = clock();
 	for (register int i = 0; i < M; i++)
-		*find = bst_op4(p, key, N);
+		*find = bst_op4(p, *(q + i), N);
 	cout << "Op4: " << clock() - start << endl;
+
+	start = clock();
+	for (register int i = 0; i < M; i++)
+		*find = bst_op5(p, *(q + i), N);
+	cout << "Op5: " << clock() - start << endl;
+
 	return 0;
 }
