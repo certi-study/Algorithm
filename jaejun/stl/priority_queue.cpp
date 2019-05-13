8735da8bc388b5925245096c56f47cfe69b5c0b5
dreamline91 <dreamline91@naver.com>
2019-05-13 20:57:01 +0900

    Update priority_queue.cpp

--- jaejun/stl/priority_queue.cpp
+++ jaejun/stl/priority_queue.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <ctime>
-#include <queue>
 using namespace std;
 
-#define N 1234567
 #define MAX_SIZE 128
 
 int heap[MAX_SIZE];
 int heapSize = 0;
-priority_queue< int, vector<int>, less<int> > pq;
+/* 이유는 모르겠지만 이 상황에서는 함수로 변수 넘기는 것 보다 전역 변수가 빠르다 */
+/* 아마 함수 인자가 많아져서인듯? */
 
 /* reference: https://www.swexpertacademy.com */
-void heapInit(void) {
+inline void heapInit(void) {
 	heapSize = 0;
 }
 
@@ -27,6 +25,7 @@ int heapPush(register int value) {
 	register int current = heapSize;
 	register int temp, idx = (current - 1) >> 1;
 	while (current > 0 && heap[current] < heap[idx]) {
+		/* swap 함수 안쓰니 시간이 반 덜걸림... */
 		temp = heap[idx];
 		heap[idx] = heap[current];
 		heap[current] = temp;
@@ -48,24 +47,23 @@ int heapPop(register int *value) {
 
 	*value = heap[0];
 	heapSize = heapSize - 1;
-
 	heap[0] = heap[heapSize];
 
 	register int current = 0;
-	register int temp, child, idx = current << 1;
-	while (idx + 1 < heapSize) {
-		child = (idx + 2 == heapSize) ? idx + 1 : (heap[idx + 1] < heap[idx + 2] ? idx + 1 : idx + 2);
+	register int temp, child, left = (current << 1) + 1, right = 0;
+	while (left < heapSize) {
+		right = left + 1;
+		child = (right == heapSize) ? left : (heap[left] < heap[right] ? left : right);
 
-		if (heap[current] < heap[child]) {
+		if (heap[current] < heap[child])
 			break;
-		}
 
 		temp = heap[current];
 		heap[current] = heap[child];
 		heap[child] = temp;
 
 		current = child;
-		idx = current << 1;
+		left = (current << 1) + 1;
 	}
 	return 1;
 }
