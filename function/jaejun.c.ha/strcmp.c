e386e147839b4b142eb31c9d555d30d0d3d0f5c0
dreamline91 <dreamline91@naver.com>
2020-09-20 17:31:36 +0900

    Update strcmp.c

--- function/jaejun.c.ha/strcmp.c
+++ function/jaejun.c.ha/strcmp.c
@@ -1,6 +1,6 @@
 /* Reference: https://na982.tistory.com */
-int strcmp(char *a, char *b){
-	int i = 0, j = 0;
+inline int strcmp(char *a, char *b){
+	register int i = 0, j = 0;
 	while(a[i]){
 		if(a[i++] != b[j++]){
 			--i, --j;
